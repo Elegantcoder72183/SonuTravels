a57bcf33fa7391c10809d2d755e7a74d36473dfb
Elegantcoder72183 <sourabh6712@gmail.com>
2024-05-17 23:11:25 +0530

    Top Destination and Form Section added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../organisms/Header'
+import PackagesForm from '../molecules/PackagesForm'
+import TopDestinations from '../molecules/TopDestinations'
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <PackagesForm />
+      <TopDestinations />
     </div>
   )
 }
--- src/organisms/Header/index.jsx
+++ src/organisms/Header/index.jsx
@@ -1,18 +1,8 @@
 import React, { useState } from "react";
 import "./Header.css";
 import Navbar from "../../molecules/Navbar";
-import PackagesForm from "../../molecules/PackagesForm";
 
 const Header = () => {
-  const [openPackagesForm, setOpenPackagesForm] = useState(false);
-
-  const handlePackagesForm = () => {
-    if (!openPackagesForm) {
-      setOpenPackagesForm(true);
-    } else {
-      setOpenPackagesForm(false);
-    }
-  };
   return (
     <>
       <Navbar />
@@ -28,17 +18,9 @@ const Header = () => {
           <p className="video-heading">Explore the World</p>
           <p className="video-sub-heading">
             Searching for Flights, Hotels, Tours, Car Bookings
-            <button onClick={handlePackagesForm}>Click here</button>
+            <button>Click here</button>
           </p>
         </div>
-        {openPackagesForm && (
-          <div className="form-container">
-            {" "}
-            <PackagesForm />{" "}
-          </div>
-        )}
-
-        {/* <button className='enquiry Button'>Need Enquiry</button> */}
       </div>
     </>
   );
