305d4d5c544737ab7f82176c7e4ae45851f2ed83
Aastha <aasthatyagi54@gmail.com>
2024-06-21 23:57:53 +0530

    Foreign destination added

---
+++ src/molecules/TopDestinations/topDestinationAbroadSchema.js
@@ -0,0 +1,30 @@
+
+const topDestinationAbroadSchema = [
+    {
+      name: "Italy",
+      image: "/images/Italy.jpg",
+    },
+    {
+      name: "America",
+      image: "/images/America.jpg",
+    },
+    {
+      name: "Greece",
+      image: "/images/Greece.jpg",
+    },
+    {
+      name: "Dubai",
+      image: "/images/Dubai.jpg",
+    },
+    {
+      name: "Singapore",
+      image: "/images/Singapore.jpg",
+    },
+    {
+      name: "Bali",
+      image: "/images/Bali.jpg",
+    },
+  ];
+  
+  export default topDestinationAbroadSchema;
+  
